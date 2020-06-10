0f4c50ea323e4ccddd5fd1d3ae9f65ef9c33c472
juannodeb <juannodeb@gmail.com>
2020-06-10 11:13:30 -0500

    Created card component w/ bootstrap

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+  <div className="card">
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+  );
+}
+
+export default Card;
